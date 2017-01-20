84339d441d7e1e6cb1c12a4c99b26da7bf9a6915
Shruti Priya <shrutipriya1808@gmail.com>
2017-01-20 12:46:43 -0800

    add code for numbers divisible by 3, 5and 15

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,46 @@
 // <!......Business logic starts here....!>
-var numberList =[1 ,2 ,3, 4, 5, 6, 7, 8, 9, 10, 11,12, 13 ,14, 15,16, 17, 18 ,19 ,20, 21, 22, 23, 24, 25,26,27,28,29,30];
+// var numberList =[1 ,2 ,3, 4, 5, 6, 7, 8, 9, 10, 11,12, 13 ,14, 15,16, 17, 18 ,19 ,20, 21, 22, 23, 24, 25,26,27,28,29,30];
 
 var pingPong =function(number){
- var resultArray = [];
- if(number === 0){
-   return alert("Please enter a valid number");
- }else if (typeof(number)=== "string") {
-   return alert("Please enter a valid value");
+  var rangeArray = [];
 
 
- }
- for (i=0 ; i <= numberList.length; i++){
-  if((numberList[i] % 3)=== 0){
-    resultArray.push("ping");
-  }else if ((numberList[i] % 5 === 0){
-   resultArray.push("pong");
- }else if (numberList[i]% 15 ===0){
-   resultArray.push("ping-pong");
-}
-
-
-return resultArray;
-
- }
-}
+  // For non-numeric Input and number less than 1
+  if(number === 0 || number < 1){
+    alert("Please enter a valid input");
+  }else if(typeof(number)=== "string") {
+    alert("Please enter a valid value");
+  }
+  else{
+    for (i=0 ; i <= number; i++){
+      // Number divisible by 3 replaced with "ping"//
+      if(i % 3 === 0){
+        rangeArray.push("ping");
+        // Number divisible by 5 replaced with "pong"//
+      }else if (i % 5 === 0){
+        rangeArray.push("pong");
+        // Number divisible by 15 replaced with "ping-pong"//
+      }else if (i % 3 && i % 5 ===0){
+        rangeArray.push("ping-pong");
+      }
+      else {
+        rangeArray.push(i);
+      }
+      var result= rangeArray.join(",");
+      return result;
+    }
+  }
+};
 
 // <!......User interface logic starts here....!>
 $(document).ready(function(){
-  $("form#ping-pong").submit(function(event){
+  $("form#user-input").submit(function(event){
     event.preventDefault();
 
-    var Input =parseInt($("input#number")).val());
+    var userInput =parseInt($("#input-number")).val());
+    // Takes user input and then converts that into an integer
+    var rangeArrayresult = pingPong(userInput);
+    $("#result").text(rangeArrayresult);
+
+   });
+ };
